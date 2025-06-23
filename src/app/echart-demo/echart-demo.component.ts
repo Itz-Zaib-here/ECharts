be2335e101639b19dac3106208bdd1080a32be86
Shah Zaib <shahzaibshazada123@gmail.com>
2025-06-23 18:27:23 +0500

    added bar chart and implement with Api

--- src/app/echart-demo/echart-demo.component.ts
+++ src/app/echart-demo/echart-demo.component.ts
@@ -15,6 +15,7 @@ export class EchartDemoComponent implements OnInit {
   dataVisible = false;
   chartOption: EChartsOption = {};
   pieChartOption: EChartsOption = {};
+  barChart: EChartsOption = {};
 
   constructor(private http: HttpClient) {}
 
@@ -102,7 +103,7 @@ export class EchartDemoComponent implements OnInit {
           ],
         };
 
-        // Pie Chart 
+        // Pie Chart
         this.pieChartOption = {
           title: {
             text: 'Top Currency Rates',
@@ -116,7 +117,7 @@ export class EchartDemoComponent implements OnInit {
             orient: 'horizontal',
             bottom: 0,
             left: 'center',
-            type: 'scroll', 
+            type: 'scroll',
             textStyle: {
               fontSize: 12,
               color: '#333',
@@ -152,6 +153,65 @@ export class EchartDemoComponent implements OnInit {
                 name: rates[key].name,
                 value: rates[key].value,
               })),
+            },
+          ],
+        };
+
+        //bar chart
+        this.barChart = {
+          title: {
+            text: 'Currency Exchange Rates',
+            left: 'center',
+            top: 10,
+            textStyle: {
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: '#333',
+            },
+          },
+          tooltip: {
+            trigger: 'axis',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            borderColor: '#ccc',
+            textStyle: {
+              color: '#fff',
+            },
+          },
+          grid: {
+            left: '5%',
+            right: '5%',
+            bottom: '10%',
+            containLabel: true,
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: labels,
+            axisLabel: {
+              rotate: 45,
+              color: '#666',
+              fontSize: 12,
+            },
+          },
+          yAxis: {
+            type: 'value',
+            axisLabel: {
+              color: '#666',
+            },
+            splitLine: {
+              lineStyle: {
+                color: '#eee',
+              },
+            },
+          },
+          series: [
+            {
+              name: 'Rate',
+              type: 'bar',
+              data: values,
+              itemStyle: {
+                color: '#4f8aff',
+              },
             },
           ],
         };
